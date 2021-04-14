3b71d4876823fb7e9b70ae556a85891b7c6da3b5
tober_r1 <2389704501@qq.com>
2021-04-15 01:11:51 +0800

    new loading

--- app/src/main/java/com/tobey/sample/MainActivity.java
+++ app/src/main/java/com/tobey/sample/MainActivity.java
@@ -9,9 +9,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.tobey.dialogloading.AlertDialogUtils;
+import com.tobey.dialogloading.Const;
 import com.tobey.dialogloading.DialogUtil;
+import com.tobey.dialogloading.GifDialog;
 import com.tobey.dialogloading.SmoothRoundProgressBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,14 +23,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // final Dialog dialog = DialogUtil.createLoadingDialog(this,"加载中...");
-        final AlertDialog dialog = AlertDialogUtils.loadingDialog(this,"登录中...");
-       AlertDialogUtils.setProgressColor(getResources().getColor(R.color.colorAccent));
+        final Dialog dialog = GifDialog.createDialog(this,"加载中");
+        GifDialog.setTextColor(R.color.colorAccent);
+       /* GifDialog.setImageBackGround(R.drawable.loading_normal,this);
+        GifDialog.setTextColor(R.color.colorAccent);
+        GifDialog.setlayoutBackGround(this.getResources().getDrawable(R.drawable.loading_bg));*/
+      //  final Dialog dialog = DialogUtil.createLoadingDialog(this,"加载中");
+     //   final AlertDialog dialog = AlertDialogUtils.loadingDialog(this,"登录中...");
+     //  AlertDialogUtils.setProgressColor(getResources().getColor(R.color.colorAccent));
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-            //    DialogUtil.closeDialog(dialog);
-           //     AlertDialogUtils.closeDialog(dialog);
+
+              //  AlertDialogUtils.closeDialog(dialog);
             }
         },5000);
 
